8bd555987c83817a0bbe8710908e3ab80243f259
MaNn0 <mlolo80013@gmail.com>
2025-03-05 21:45:30 +0400

    enhancing style

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,6 +1,7 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
+
 interface FilterProps {
     onSort: (order: "asc" | "desc") => void;
 }
@@ -11,7 +12,7 @@ export default function Filter({ onSort }: FilterProps) {
             <div>
                 <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-7 py-2 text-md font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50">
                     Filter
-                    <ChevronDownIcon aria-hidden="true" className="-mr-1 text-gray-400" />
+                    <ChevronDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
                 </MenuButton>
             </div>
 
